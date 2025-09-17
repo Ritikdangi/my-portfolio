10c2fe436f0890de5d48cf6eaa814dd63f86dad8
Ritik dangi <ritikdangi445566@gmail.com>
2025-09-18 01:06:56 +0530

    added avatar pic for navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <motion.nav
       className="fixed top-6 left-1/2 -translate-x-1/2 
                  w-[95%] sm:w-[90%] lg:w-[80%] 
-                 rounded-full bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg 
+                 rounded-full  backdrop-blur-lg border border-white/20 shadow-lg 
                  px-4 sm:px-6 py-3 z-50"
       variants={container}
       initial="hidden"
@@ -16,14 +16,13 @@ export default function Navbar() {
     >
       <div className="w-full max-w-7xl mx-auto flex items-center justify-between">
         {/* Avatar / Logo */}
-        <motion.div variants={item} className="flex items-center gap-2">
-          <motion.img
-            src="/avatar.png"
-            alt="logo"
-            className="w-9 h-9 sm:w-10 sm:h-10 rounded-full border border-white/30"
-          />
-        </motion.div>
-
+        <motion.div variants={item} className="flex items-end gap-2 h-full">
+  <motion.img
+    src="/avatar.png"
+    alt="logo"
+    className="h-10 sm:h-12 object-contain "
+  />
+</motion.div>
         {/* Nav Links */}
         <motion.ul
           className="hidden sm:flex gap-6 md:gap-8 text-white font-mono text-xs sm:text-sm"
